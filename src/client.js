95616b7bbb38e809e5d37dc5d26f57380f97d34a
covidy <vangelistewilner@outlook.com>
2021-12-25 04:41:41 +0100

    adding in studio

---
+++ src/client.js
@@ -0,0 +1,7 @@
+import sanityClient from '@sanity-client'
+
+
+export default sanityClient({
+  projectId: "m7j48r8i",
+  databaset: "wilfolio"
+})
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Home() {
+  return (
+    <main>
+      <img alt=""/>
+      <section>
+        <h1>Hi, I'm Wilner.</h1>
+      </section>
+    </main>
+  )
+}
